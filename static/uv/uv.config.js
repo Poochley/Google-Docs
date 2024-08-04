eeeb3df2a3eddc084b30a5e5a210ca0fd2799ff5
Poochley <poochley123@proton.me>
2024-08-04 12:10:41 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/education/',
-    bare: '/googles/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
